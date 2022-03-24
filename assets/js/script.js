1758e911e81f0d2c9f720d3dfe766de7af0017ae
Mikekuney <96913841+Mikekuney@users.noreply.github.com>
2022-03-24 11:23:59 -0400

    added qustion and answer functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,7 +11,7 @@ var highScoresList = document.querySelector(".highscores")
 // global variables
 var timer;
 var timeDown;
-var timeCounter;
+var timeCount;
 var newQuestion;
 var ulListEl;
 var listItemEl;
@@ -63,5 +63,104 @@ var questions = [
         answer: ["()", "<>", "[]", "{}"],
         correctAnswer: 2,
         questionNum: 4
-    },
-]
+    }
+];
+
+// start game 
+
+startButton.addEventListener("click", startGame);
+
+function startGame() {
+    guideline.style.display = "none";
+    secondsLeft.style.display = "block";
+    startButton.style.display = "block";
+    questionsText.style.display = "block";
+    listItemEl.style.display = "block";
+    listItemEl.style.backgroundColor = "transparent";
+    newQuestion = 0;
+    timer = 30;
+    timeCount = setInterval(timeDown, 1000);
+
+    questionFunction();
+};
+
+// count down 
+function timeDown() {
+    timer;
+    if(timer > 0) {
+        timer--;
+    }
+    else {
+        clearInterval(timeCount);
+        return endGame();
+    }
+    clock.innerHTML = timer;
+};
+
+// create elements
+
+function createItem(element, className) {
+    var newEl = document.createElement(element);
+    newEl.setAttribute("class", className);
+    return newEl;
+};
+
+// creating El list
+
+ulListEl = createItem("ul", "ulListElName")
+ulListEl.style.display = "none";
+listItemEl = createItem("li", "listItemElName");
+listItemEl.style.listStyle = "none";
+
+// buttons list
+
+buttonOne = createItem("button", "answerButton");
+buttonTwo = createItem("button", "answerButton");
+buttonThree = createItem("button", "answerButton");
+buttonFour = createItem("button", "answerButton");
+var playAgain = createItem("h3", "playAgainText");
+playAgain.innerHTML = "Play Again?";
+
+// append child list
+
+buttonsList.appendChild(ulListEl);
+ulListEl.appendChild(listItemEl);
+listItemEl.appendChild(buttonOne);
+listItemEl.appendChild(buttonTwo);
+listItemEl.appendChild(buttonThree);
+listItemEl.appendChild(buttonFour);
+
+ulListEl.appendChild(playAgain);
+
+// question function
+function questionFunction() {
+    startButton.style.display = "none";
+    playAgain.style.display = "none";
+    var theQuestion = questions[newQuestion];
+    questionsText.innerHTML = theQuestion.question;
+    answers();
+};
+
+//answer function
+function answers() {
+    ulListEl.style.display = "block";
+    var buttonAnswer = questions[newQuestion].answers;
+    buttonOne.innerHTML = buttonAnswer[0];
+    buttonTwo.innerHTML = buttonAnswer[1];
+    buttonThree.innerHTML = buttonAnswer[2];
+    buttonFour.innerHTML = buttonAnswer[3];
+};
+
+var checkAnswers;
+var buttonClick = document.querySelectorAll(".answerButton").forEach(element => {
+    element.addEventListener("click", event => {
+        checkAnswer = element.innerHTML;
+        rightOrWrong();
+    })
+});
+
+// if else for correct/incorrect answers
+function rightOrWrong()
+
+// timer interval clear and shows user score
+function endGame()
